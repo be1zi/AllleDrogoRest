eb52a420aee9c401cb64c67068753ea2b0e96e01
be1zi <konrad.belzowski@gmail.com>
2017-12-23 20:33:22 +0100

    my_auction

--- src/main/java/Rest/Controller/SaleController.java
+++ src/main/java/Rest/Controller/SaleController.java
@@ -4,6 +4,7 @@ import Rest.DAO.AuctionRepository;
 import Rest.DAO.PhotoRepository;
 import Rest.Model.AuctionModel;
 import Rest.Model.PhotoModel;
+import Rest.Model.UserModel;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static java.lang.System.out;
 
@@ -32,11 +36,28 @@ public class SaleController {
 
         for( int i = 0; i<auctionModel.getFiles().size(); i++) {
             PhotoModel pM = auctionModel.getFiles().get(i);
-            out.println(pM.toString());
             photoRepository.save(pM);
         }
         auctionRepository.save(auctionModel);
         return new ResponseEntity<>(auctionModel, new HttpHeaders(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/getMyAuction", method = RequestMethod.POST)
+    public ResponseEntity<AuctionModel[]> getMyAuction(@RequestBody UserModel userModel){
+
+        List<AuctionModel> aMList = auctionRepository.findAllByUserId(userModel.getId());
+
+        AuctionModel[] result = new AuctionModel[aMList.size()];
+
+        for(int i = 0; i<aMList.size(); i++){
+            result[i] = aMList.get(i);
+        }
+
+        if(aMList == null)
+            return new ResponseEntity<>(result, new HttpHeaders(), HttpStatus.valueOf(301));
+        else
+            return new ResponseEntity<>(result, new HttpHeaders(), HttpStatus.OK);
+    }
+
 
 }
--- src/main/java/Rest/DAO/AuctionRepository.java
+++ src/main/java/Rest/DAO/AuctionRepository.java
@@ -3,5 +3,10 @@ package Rest.DAO;
 import Rest.Model.AuctionModel;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AuctionRepository extends CrudRepository<AuctionModel, Long> {
+
+    List<AuctionModel> findAllByUserId(Long id);
+
 }
